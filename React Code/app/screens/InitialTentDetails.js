d2b3e8199bff0383c14675317bab8b4af804c183
Scott Kopczynski <skopczynski@zagmail.gonzaga.edu>
2018-10-30 13:55:08 -0700

    tent name page

--- React Code/app/screens/InitialTentDetails.js
+++ React Code/app/screens/InitialTentDetails.js
@@ -1,22 +1,81 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  ScrollView
-} from 'react-native';
+import {Text,View,ScrollView,StyleSheet,TextInput, TouchableOpacity} from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
 
 class InitialTentDetails extends Component {
+  state = {
+    tentName: '',
+    lastName: '',
+    pin: ''
+  }
+  handleTentName = (text) => {
+    this.setState({ tentName: text })
+  }
+  handleTentName = (text) => {
+    this.setState({ lastName: text })
+  }
+  handlePin = (text) => {
+    this.setState({ pin: text })
+  }
+  login = (tentName, lastName, pin) => {
+    alert('tentName: ' + tentName + ' lastName: ' + lastName + 'Pin:' + pin)
+ }
   render() {
     return (
       <Grid>
-        <Row size={30}></Row>
-        <Row size={40}>
-          <Col size={30}></Col>
-          <Col size={40}><Text style={{fontSize: 50}}>Tent Details</Text></Col>
-          <Col size={30}></Col>
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <TextInput style = {styles.input}
+                  placeholder = "Tent Name"
+                  placeholderTextColor = "black"
+                  autoCapitalize = "none"
+                  onChangeText = {this.handleTentName}/>
+          </Col>
+          <Col size={10}></Col>
         </Row>
-        <Row size={30}></Row>
+        <Row size={5}>
+        </Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <TextInput style = {styles.input}
+                  placeholder = "Last Name"
+                  placeholderTextColor = "black"
+                  autoCapitalize = "none"
+                  onChangeText = {this.handleLastName}/>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={5}></Row>
+        <Row size={10}>
+        <Col size={10}></Col>
+          <Col size={80}>
+            <TextInput style = {styles.input}
+                  placeholder = "Pin"
+                  placeholderTextColor = "black"
+                  keyboardType = 'number-pad'
+                  secureTextEntry = {true}
+                  autoCapitalize = "none"
+                  onChangeText = {this.handlePin}/>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={35}></Row>
+        <Row size={20}>
+          <Col size={20}></Col>
+            <Col size={60}>
+              <View style = {styles.container}>
+              <TouchableOpacity onPress={() => this.login(this.state.tentName, this.state.lastName, this.state.pin)}>
+                  <Text style = {styles.text}>
+                    Submit
+                  </Text>
+              </TouchableOpacity>
+              </View>
+            </Col>
+            <Col size={20}></Col>
+        </Row>
       </Grid>
       
     );
@@ -24,3 +83,23 @@ class InitialTentDetails extends Component {
 }
 
 export default InitialTentDetails;
+
+const styles = StyleSheet.create({
+  input: {
+     textAlign: 'center',
+     height: 40,
+     borderColor: 'black',
+     borderWidth: 1,
+     width: '100%'
+  },
+  container: {
+    alignItems: 'center',
+    width: '100%'
+ },
+ text: {
+    borderWidth: 1,
+    padding: 25,
+    borderColor: 'black',
+    fontSize: 30
+ },
+})
