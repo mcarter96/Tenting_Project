aad255fb76c360570dfb5f5f7d675bbc829e37e7
Scott Kopczynski <skopczynski@zagmail.gonzaga.edu>
2018-11-04 12:19:40 -0800

    Finished qr screen

--- React Code/app/screens/QRScreen.js
+++ React Code/app/screens/QRScreen.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import {Text,View,ScrollView} from 'react-native';
+import {Text,View,ScrollView, StyleSheet} from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import QRCode from 'react-native-qrcode';
 
 class QRScreen extends Component {
+  state = {
+    tentNumber: '_____',
+  }
   render() {
+    const { navigation } = this.props;
+    const tentMembers = navigation.getParam('tentMembers', 'bad');
     return (
       <Grid>
-        <Row size={30}></Row>
-        <Row size={40}>
-          <Text style={{fontSize: 50}}>Display QR Code here!</Text>
+        <Row size={10}></Row>
+        <Row size={20}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+              <Text style={{fontSize: 50}}>Tent #{this.state.tentNumber}</Text>
+            </View>
+          </Col>
+          <Col size={10}></Col>
         </Row>
-        <Row size={30}></Row>
+        <Row size={30}>
+          <Col size={24}></Col>
+          <Col size={52}><QRCode
+            value={tentMembers[0]}
+            size={200}
+            bgColor='black'
+            fgColor='white'/></Col>
+          <Col size={24}></Col>
+        </Row>
+        <Row size={10}></Row>
+        <Row size={20}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+              <Text style={{fontSize: 50}}>Countdown!</Text>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={10}></Row>
       </Grid>
       
     );
@@ -19,3 +49,25 @@ class QRScreen extends Component {
 }
 
 export default QRScreen;
+
+const styles = StyleSheet.create ({
+  container: {
+     alignItems: 'center',
+     width: '100%'
+  },
+  text: {
+     borderWidth: 1,
+     padding: 25,
+     borderColor: 'black',
+     fontSize: 30
+  },
+  textJoin: {
+    borderWidth: 1,
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 25,
+    paddingBottom: 25,
+    borderColor: 'black',
+    fontSize: 30
+ },
+});
