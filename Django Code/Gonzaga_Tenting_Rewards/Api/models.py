f334f351860d861b2cd3867533a98cc3db0fe568
Carlos Villagomez <villagoc@gmail.com>
2018-11-19 11:06:57 -0800

    Tenting group model/tests completed

--- Django Code/Gonzaga_Tenting_Rewards/Api/models.py
+++ Django Code/Gonzaga_Tenting_Rewards/Api/models.py
@@ -101,6 +101,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 class TentGroup(models.Model):
+    """Creates a new instance of a tenting group object and assigns up to 6 users to the group"""
     tenter_1 = models.ForeignKey(UserProfile, related_name='tenter_1', on_delete=models.CASCADE,)
     tenter_2 = models.ForeignKey(UserProfile, related_name='tenter_2', on_delete=models.CASCADE,)
     tenter_3 = models.ForeignKey(UserProfile, related_name='tenter_3', on_delete=models.CASCADE,)
@@ -111,7 +112,7 @@ class TentGroup(models.Model):
     qr_code_str = models.CharField(max_length=100)
 
     def create_tent_group(self, tenter_1, tenter_2, tenter_3, tenter_4, tenter_5, tenter_6, tent_pin, qr_code_str):
-        """Creates a new tent group object."""
+        """Creates a new tenting group object."""
         tent_group = self.model(tenter_1=tenter_1, tenter_2=tenter_2, tenter_3=tenter_3, tenter_4=tenter_4, tenter_5=tenter_5, tenter_6=tenter_6, tent_pin=tent_pin, qr_code_str=qr_code_str)
 
         return tent_group
