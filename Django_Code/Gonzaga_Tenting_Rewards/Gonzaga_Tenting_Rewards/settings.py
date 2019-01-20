7fd43f46b6123780dd977914546164f7bb0658d7
Carlos Villagomez <villagoc@gmail.com>
2019-01-20 14:05:58 -0800

    Added password reset feature

--- Django_Code/Gonzaga_Tenting_Rewards/Gonzaga_Tenting_Rewards/settings.py
+++ Django_Code/Gonzaga_Tenting_Rewards/Gonzaga_Tenting_Rewards/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'Api',
 ]
 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -137,3 +140,11 @@ else:
 
 # Overrides the current user to use our own custom user
 AUTH_USER_MODEL = 'Api.UserProfile'
+
+# Configures an SMTP Email Backend
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tenting.rewards@gmail.com'
+EMAIL_HOST_PASSWORD = 'tentingRewards'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'TestSite Team <noreply@example.com>'
