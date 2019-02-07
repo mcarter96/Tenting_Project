12fbccaab438a4664900557535025363cb27beb4
mcarter96 <m6carter@comcast.net>
2019-02-07 11:47:11 -0800

    working on style

--- React_Code/app/screens/Settings.js
+++ React_Code/app/screens/Settings.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  ScrollView
+  ScrollView,
+  StyleSheet
 } from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
+
 
 class Settings extends Component {
   render() {
     return (
-      <Grid>
-        <Row size={30}></Row>
-        <Row size={40}>
-          <Col size={20}></Col>
-          <Col size={60}><Text style={{fontSize: 50}}>Settings</Text></Col>
-          <Col size={20}></Col>
-        </Row>
-        <Row size={30}></Row>
+       <Grid>
+         <Row size={30}></Row>
+         <Row size={40}>
+           <Col size={20}></Col>
+          <Col size={60}><Text style={styles.myState}>Settings</Text></Col>
+           <Col size={20}></Col>
+         </Row>
+         <Row size={30}></Row>
       </Grid>
-      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  myState: {
+    marginTop: 20,
+    textAlign: 'center',
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20
+  }
+});
 
 export default Settings;
