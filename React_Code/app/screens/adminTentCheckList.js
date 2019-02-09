7b1c28021975ca3300fa498946355400fa8a84e2
Scott Kopczynski <skopczynski@zagmail.gonzaga.edu>
2019-02-09 15:26:31 -0800

    added initial tent check page

---
+++ React_Code/app/screens/adminTentCheckList.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import CheckboxFormX from 'react-native-checkbox-form';
+import { Col, Row, Grid } from "react-native-easy-grid";
+const mockData = [
+  {
+      label: 'label1',
+      value: 'one'
+  },
+  {
+      label: 'label2',
+      value: 'two'
+  },
+  {
+      label: 'label3',
+      value: 'three'
+  },
+];
+class CheckList extends Component {
+  state = {
+    tentId: '',
+  }
+  _onSelect = ( item ) => {
+    console.log(item);
+  };
+  componentDidMount(){
+    const { navigation } = this.props;
+    const tentid = navigation.getParam('tentid', 'No ID');
+    this.setState({tentId: tentid});
+};
+  render() {
+    return (
+      <Grid>
+        <Row size={30}>
+        <View style={styles.container}>
+          <View style={{ marginVertical: 10, backgroundColor: "#E7E7E7" }} >
+              <CheckboxFormX
+                  style={{ width: 350 - 30 }}
+                  dataSource={mockData}
+                  itemShowKey="label"
+                  itemCheckedKey="RNchecked"
+                  iconSize={16}
+                  formHorizontal={true}
+                  labelHorizontal={false}
+                  onChecked={(item) => this._onSelect(item)}
+              />
+          </View>
+     </View>
+        </Row>
+        <Row size={40}>
+        </Row>
+        <Row size={30}></Row>
+      </Grid>
+      
+    );
+  }
+}
+
+export default CheckList;
+
+const styles = StyleSheet.create ({
+  container: {
+     //alignItems: 'center',
+     width: '100%'
+  },
+  containerOne:{
+      alignItems: 'center',
+      width: '100%'
+  },
+  text: {
+      borderWidth: 1,
+      paddingTop: 10,
+      paddingBottom: 10,
+      paddingLeft:30,
+      paddingRight:30,
+      borderColor: 'black',
+      fontSize: 20
+  },
+  textJoin: {
+    borderWidth: 1,
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 25,
+    paddingBottom: 25,
+    borderColor: 'black',
+    fontSize: 30
+ },
+})
