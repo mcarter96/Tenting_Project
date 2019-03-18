2c4321c2e454a4d6a603270be47efd40cc73ad0d
Scott Kopczynski <skopczynski@zagmail.gonzaga.edu>
2019-03-17 19:14:31 -0700

    Finished filter tents front end

--- React_Code/app/screens/adminTentFilter.js
+++ React_Code/app/screens/adminTentFilter.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  ScrollView
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
 
@@ -113,6 +115,69 @@ class adminTentFilter extends Component {
         }
         return result;
       }
+      missingSetup = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(1);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      }
+      missingWaiver = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(2);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      }
+      missingCheckOne = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(3);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      }
+      missingCheckTwo = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(4);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      } 
+      missingCheckThree = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(5);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      }
+      missingCheckFour = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(6);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      }
+      missingFinalCheck = () =>{
+        var alertString = "Tents:\n" 
+        var result = this.getMissingTentCheck(7);
+        for(var i = 0; i < result.length; i++){
+          alertString += String(result[i])
+          alertString += "\n"
+        }
+        alert(alertString)
+      }
      async componentDidMount(){
       const { navigation } = this.props;
       const adminToken = navigation.getParam('token', 'No ID');
@@ -120,19 +185,109 @@ class adminTentFilter extends Component {
       var result =  await this.getFilteredTents(adminToken)
       await this.tentIdstoNumbers(adminToken);
       this.setState({tentData:result});
-      console.log(this.getMissingTentCheck(4))
-
      }
   render() {
     return (
       <Grid style={{backgroundColor: "#639aff"}}>
-        <Row size={30}></Row>
-        <Row size={40}>
-          <Col size={30}></Col>
-          <Col size={40}><Text style={{fontSize: 50}}>Admin Feed</Text></Col>
-          <Col size={30}></Col>
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingSetup}>
+                <Text style = {styles.text}>
+                  Missing Setup
+                </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
         </Row>
-        <Row size={30}></Row>
+
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingWaiver}>
+              <Text style = {styles.text2}>
+                Missing Waiver
+              </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>        
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingCheckOne}>
+                <Text style = {styles.text4}>
+                  Missing Check 1
+                </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingCheckTwo}>
+                <Text style = {styles.text4}>
+                  Missing Check 2
+                </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingCheckThree}>
+                <Text style = {styles.text4}>
+                  Missing Check 3
+                </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingCheckFour}>
+                <Text style = {styles.text4}>
+                  Missing Check 4
+                </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
+        <Row size={5}></Row>
+        <Row size={10}>
+          <Col size={10}></Col>
+          <Col size={80}>
+            <View style = {styles.container}>
+            <TouchableOpacity onPress={this.missingFinalCheck}>
+                <Text style = {styles.text3}>
+                  Missing Final Check
+                </Text>
+            </TouchableOpacity>
+            </View>
+          </Col>
+          <Col size={10}></Col>
+        </Row>
       </Grid>
       
     );
@@ -140,3 +295,73 @@ class adminTentFilter extends Component {
 }
 
 export default adminTentFilter;
+const styles = StyleSheet.create({
+  input: {
+    color: 'white',
+    backgroundColor: '#639aff',
+    borderRadius: 10,
+    textAlign: 'center',
+    height: 40,
+    borderColor: 'white',
+    borderWidth: 0.5,
+    width: '100%'
+ },
+  container: {
+    alignItems: 'center',
+    width: '100%'
+ },
+ text: {
+  color: 'white',
+  backgroundColor: '#9aadce',
+  overflow: 'hidden',
+  borderRadius: 10,
+  borderWidth: 0,
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingLeft:60,
+  paddingRight: 60,
+  borderColor: 'black',
+  fontSize: 20
+ },
+ text4: {
+  color: 'white',
+  backgroundColor: '#9aadce',
+  overflow: 'hidden',
+  borderRadius: 10,
+  borderWidth: 0,
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingLeft:50,
+  paddingRight: 50,
+  borderColor: 'black',
+  fontSize: 20
+ },
+ text2: {
+  color: 'white',
+  backgroundColor: '#9aadce',
+  overflow: 'hidden',
+  borderRadius: 10,
+  borderWidth: 0,
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingLeft:55,
+  paddingRight: 55,
+  borderColor: 'black',
+  fontSize: 20
+   
+},
+text3: {
+  color: 'white',
+  backgroundColor: '#9aadce',
+  overflow: 'hidden',
+  borderRadius: 10,
+  borderWidth: 0,
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingLeft:35,
+  paddingRight: 35,
+  borderColor: 'black',
+  fontSize: 20
+
+},
+})
