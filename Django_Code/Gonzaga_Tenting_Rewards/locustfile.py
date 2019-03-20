ed995b89ac03c20d0066cae57c7ea9d440e5808c
Andrew <andrew@zenoni.com>
2019-03-20 12:15:07 -0700

    added locus stuff

--- Django_Code/Gonzaga_Tenting_Rewards/locustfile.py
+++ Django_Code/Gonzaga_Tenting_Rewards/locustfile.py
@@ -1,12 +1,38 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet, task, TaskSequence, seq_task
+import json
 
-class MyTaskSet(TaskSet):
+class LoginTaskSet(TaskSequence):
+
+    token = None
+
+    @seq_task(1)
+    def login(self):
+        request = self.client.post("/api/login/", json={"username": "test3@zagmail.gonzaga.edu", "password": "p"}, )
+        data = None
+        if request is not None and request.content is not None:
+            data = json.loads(request.content.decode('utf-8'))
+        # print(data)
+        if data is not None:
+            self.token = 'Token %s' % (data['token'])
+
+    @seq_task(2)
+    def get_profiles(self):
+        if self.token is not None:
+            self.client.get("/api/profile", headers={'Authorization': self.token})
+
+
+class ViewProfiles(TaskSet):
+
+    token = 'b01bea9ea80a09af8bd53fa422618e25b38e9e9d'
+
     @task
-    def my_task(self):
-        self.client.get("/api/", )
+    def profiles(self):
+        if self.token is not None:
+            self.client.get("/api/profile", headers={'Authorization': self.token})
+
 
 class MyLocust(HttpLocust):
-    task_set = MyTaskSet
+    task_set = ViewProfiles
     min_wait = 1000
     max_wait = 5000
 
