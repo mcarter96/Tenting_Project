ce9b7ff4b2904fb0dc127c278d21ba11a568f634
Andrew <andrew@zenoni.com>
2019-04-10 21:40:24 -0700

    modified code to attempt speed up

--- Django_Code/Gonzaga_Tenting_Rewards/Generic_Information/views.py
+++ Django_Code/Gonzaga_Tenting_Rewards/Generic_Information/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def privacy_policy(request):
     return render(request, 'privacy_policy.html')
+
+def basic_info(request):
+    return render(request, 'basic_info.html')
